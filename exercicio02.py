d439123a032fe3c56d507ba4241ac4d1d945eb57
⚡Felipe⚡ <109535066+felipedasilvamelo@users.noreply.github.com>
2023-03-10 13:57:38 -0300

    exercicio 01

---
+++ exercicio02.py
@@ -0,0 +1,31 @@
+print('-*'*20)
+
+print('Sequência de Fibonacci')
+
+print('-*'*20)
+
+n = int(input('Digite um número: '))
+
+t1 = 0
+
+t2 = 1
+
+print('-'*30)
+
+print('{} -> {}'.format(t1, t2), end = '')
+
+cont = 3
+
+while cont <= n:
+
+   t3 = t1 + t2
+
+   print('-> {}'.format(t3), end = '')
+
+   t1 = t2
+
+   t2 = t3
+
+   cont += 1
+
+print('-> FIM')
---
+++ exercicio05.py
@@ -0,0 +1,7 @@
+txt = "Texte para inverter"
+print(txt[::-1])
+
+def inverter(txt):
+    return txt[::-1]
+print(inverter("Desenvolvedor"))
+print(inverter("Apenas um Dev codando"))
